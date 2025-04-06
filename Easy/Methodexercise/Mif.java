11af18c44d0f6abe67ff4d72a98ba3a06dc61309
vansh Dhumal <158541510+Vansh24endl@users.noreply.github.com>
2025-04-06 23:09:29 +0530

    Mif

---
+++ Easy/Methodexercise/Mif.java
@@ -0,0 +1,16 @@
+public class Mif{
+ static void checkage(int age){
+   if(age <18)
+   {
+     System.out.println("Acess denined");
+   }
+   else
+   {
+     System.out.println("Acess granted");
+   }
+ }
+  
+  public static void main(String args[]){
+  checkAge(20);
+  }
+}
