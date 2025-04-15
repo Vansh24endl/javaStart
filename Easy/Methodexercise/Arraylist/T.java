625bb2bff4fd3026351011307a1da596f1cb4822
vansh Dhumal <158541510+Vansh24endl@users.noreply.github.com>
2025-04-15 21:23:21 +0530

    Create T.java

---
+++ Easy/Methodexercise/Arraylist/T.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+public class T
+  {
+    public static void main(String args[])
+    {
+      ArrayList al = new ArrayList();
+      al.add(10);
+      al.add("black");
+      al.add(true);
+      al.add(null);
+
+      System.out.println(al);
+      for(int i = 0; i <al.size(); i++)
+        {
+          System.out.println(al.get(i));
+        }
+       al.remove(0);
+       al.set(2,"green");
+      System.out.println(al);
+      System.out.println("Index of black object: " + al.firstIndexof(10);
+    }
+  }
